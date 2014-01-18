414f6cde4482f08ccb9f31b75aa13cf789aacfbf
Mike Ostenberg <mostenberg@soasta.com>
2014-01-18 00:55:09 -0800

    Changed include locations

--- parseSOASTAResultsSummary.pl
+++ parseSOASTAResultsSummary.pl
@@ -18,8 +18,8 @@ use lib './libwww-perl-master/lib'; #When launching on CloudBees, this will tell
 use lib './libwww-perl-master/lib/LWP'; #When launching on CloudBees, this will tell it where to find the HTTP libraries.
 use lib './SOASTA/libwww-perl-master/lib/LWP'; #When launching on CloudBees, this will tell it where to find the HTTP libraries.
 use lib './SOASTA/libwww-perl-master/lib/'; #When launching on CloudBees, this will tell it where to find the HTTP libraries.
-use lib './libwww-perl/lib/LWP'; #When launching on CloudBees, this will tell it where to find the HTTP libraries.
-use lib './http-message-master/lib/HTTP'; #When launching on CloudBees, this will tell it where to find the HTTP libraries.
+use lib './SOASTA/libwww-perl/lib/LWP'; #When launching on CloudBees, this will tell it where to find the HTTP libraries.
+use lib './SOASTA/http-message-master/lib/HTTP'; #When launching on CloudBees, this will tell it where to find the HTTP libraries.
 use LWP::UserAgent;
 use HTTP::Request;
 local %shouldPlot;
